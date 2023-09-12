9b66e96c92195168f4f0da326bed864c2fa2b80d
Muqtadirat <muqtadir.my@gmail.com>
2023-09-12 03:00:17 +0100

    Setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import SearchBar from "./SearchBar";
+import MovieList from "./MovieList";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const apiUrl =
+  "https://api.themoviedb.org/3/movie/popular?api_key=83f937cdd12135813452398bf880e27c";
+const apiSearch =
+  "https://api.themoviedb.org/3/search/movie?api_key=83f937cdd12135813452398bf880e27c&query";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }, []);
+
+    const searchMovie = async (e) => {
+      e.preventDefault();
+      console.log("Searching");
+      try {
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=83f937cdd12135813452398bf880e27c&query=${query}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+        setMovies(data.results);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    const changeHandler = (e) => {
+      setQuery(e.target.value);
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello there </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Pass the necessary props to the SearchBar component */}
+      <SearchBar
+        searchMovie={searchMovie}
+        query={query}
+        changeHandler={changeHandler}
+        movies={movies}
+      />
+      {movies.length > 0 && <MovieList movies={movies} />}
     </div>
   );
 }
