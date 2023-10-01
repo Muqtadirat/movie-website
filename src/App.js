16ce7f1f8aeebd6a68b15c75c389c7e3eeffb341
Muqtadirat <muqtadir.my@gmail.com>
2023-10-01 12:19:47 +0100

    Minor update

--- src/App.js
+++ src/App.js
@@ -1,57 +1,24 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import SearchBar from "./SearchBar";
-import MovieList from "./MovieList";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import SearchResultsPage from "./components/SearchResultsPage";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const apiUrl =
-  "https://api.themoviedb.org/3/movie/popular?api_key=83f937cdd12135813452398bf880e27c";
-const apiSearch =
-  "https://api.themoviedb.org/3/search/movie?api_key=83f937cdd12135813452398bf880e27c&query";
-
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [query, setQuery] = useState("");
-
-  useEffect(() => {
-    fetch(apiUrl)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setMovies(data.results);
-      });
-  }, []);
-
-    const searchMovie = async (e) => {
-      e.preventDefault();
-      console.log("Searching");
-      try {
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=83f937cdd12135813452398bf880e27c&query=${query}`;
-        const res = await fetch(url);
-        const data = await res.json();
-        console.log(data);
-        setMovies(data.results);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-
-    const changeHandler = (e) => {
-      setQuery(e.target.value);
-    };
-
   return (
-    <div className="App">
-      {/* Pass the necessary props to the SearchBar component */}
-      <SearchBar
-        searchMovie={searchMovie}
-        query={query}
-        changeHandler={changeHandler}
-        movies={movies}
-      />
-      {movies.length > 0 && <MovieList movies={movies} />}
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/search-results/:query"
+            element={<SearchResultsPage />}
+          />{" "}
+          {/* New route for search results */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
